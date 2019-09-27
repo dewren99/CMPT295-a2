1a6eaec170e723f0e15e2e462151c5f5fa51cbfd
Deniz Eren Evrendilek <devrendilek@gmail.com>
2019-09-26 19:49:45 -0700

    added masking

--- sqrt.s
+++ sqrt.s
@@ -2,23 +2,28 @@
 sqrt:
 	movl $0, %eax
 	movl $15, %ecx
+	movl $0x8000, %esi
 
 loop:
 	cmpl $0, %ecx
 	jle endl
 	
 
-	or $0x8000, %eax
+	or %esi, %eax
 	movl %eax, %edx 
 
 	imull %eax, %edx
 
 	cmpl %edi, %edx
 	jg restore
+	
 	dec %ecx
+	shr $1, %esi
 	jmp loop
 
 restore:
+	xor %esi, %eax
+	shr $1, %esi
 	dec %ecx
 	jmp loop
 
